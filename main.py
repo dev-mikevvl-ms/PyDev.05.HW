ac1d0b87404dfc445e43c010e8a114805ea6e92b
mikev@W10HLEN <dev.mikevvl@outlook.com>
2022-01-28 18:10:34 +0400

    RStruApp; CheRun(HW\mod\victory.py)

--- main.py
+++ main.py
@@ -3,17 +3,12 @@
 """
 # 1. Импорт конкретных функций из модуля
 # from <название модуля> import нужные функций через ,
-from mod.famous_persons import get_random_person, get_person_and_question
+# from <название модуля> import нужные функций через ,
+import mod.MVVlStd, mod.victory 
 
 
 def main(laArgs: list[str]) -> None:
-
-  loTimes_i = int(input('Сколько раз вы хотите играть?'))
-
-  for l_co in range(loTimes_i):
-      get_person_and_question()
-
-  print('Пока!')
+  mod.victory.main(None)
 
 if __name__ == '__main__':
     # import sys
--- mod/use_functions.py
+++ mod/use_functions.py
@@ -1,101 +1,77 @@
 import sys
-from MVVlStd import teSep_s, teInPWTypedWVali_fif
+from MVVlStd import glSep_s, inp_FltAVali_feif
+# from MVVlStd import glSep_s, inp_FltAVali_feif
+# import MVVlStd
+# # MVVlStd.inp_FltAVali_feif('?')
+# # inp_FltAVali_feif('?')
 
-tTskMsg_s = '''
-Задача 5. МОДУЛЬ 3 файл use_functions.py
-В файле дано описание программы. Предлагается реализовать программу
- по описанию используя любые средства
-'''
-print(teSep_s, tTskMsg_s, f'{sys.version=}', teSep_s, 'Result:', '',  sep='\n')
-"""
-МОДУЛЬ 3
-Программа "Личный счет"
-Описание работы программы:
-Пользователь запускает программу у него на счету 0
-Программа предлагает следующие варианты действий
-1. пополнить счет
-2. покупка
-3. история покупок
-4. выход
+# tTskMsg_s = '''
+# Задача 5. МОДУЛЬ 3 файл use_functions.py
+# В файле дано описание программы. Предлагается реализовать программу
+#  по описанию используя любые средства
+# '''
+# print(glSep_s, tTskMsg_s, f'{sys.version=}', glSep_s, 'Result:', '',  sep='\n')
 
-1. пополнение счета
-при выборе этого пункта пользователю предлагается ввести сумму на сколько пополнить счет
-после того как пользователь вводит сумму она добавляется к счету
-снова попадаем в основное меню
+def main(laArgs: list[str]) -> None:
+  loAccSum_n, loHstT_l = 0, []
 
-2. покупка
-при выборе этого пункта пользователю предлагается ввести сумму покупки
-если она больше количества денег на счете, то сообщаем что денег не хватает и переходим в основное меню
-если денег достаточно предлагаем пользователю ввести название покупки, например (еда)
-снимаем деньги со счета
-сохраняем покупку в историю
-выходим в основное меню
+  def tRefillAcc_f():
+    global loAccSum_n, loHstT_l
+    loAdd_n = inp_FltAVali_feif(f' Ведите сумму на сколько пополнить счет',
+        laInPTypeFlt_cll=float, laDfV_s='100',
+        laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой',
+        laVali_cll=lambda _n: 0 <= _n)[0]
+    loAccSum_n += loAdd_n #DVL: input by inp_FltAVali_feif
+    # print(f'DBG: На счету:({loAccSum_n:.2f}) и в истории покупок {len(loHstT_l)} зап.')
+    return True
 
-3. история покупок
-выводим историю покупок пользователя (название и сумму)
-возвращаемся в основное меню
+  def tBuy_f():
+    global loAccSum_n, loHstT_l
+    loCost_n = inp_FltAVali_feif(f' Введите сумму покупки (на Вашем счету:{loAccSum_n:.2f})',
+        laInPTypeFlt_cll=float, laDfV_s=str(min(100, loAccSum_n)),
+        laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой',
+        laVali_cll=lambda _n: 0 <= _n)[0]
 
-4. выход
-выход из программы
+    if loAccSum_n < loCost_n: #DVL: input by inp_FltAVali_feif
+      print(f'Денег на Вашем счету:({loAccSum_n:.2f}) не хватает',
+          f'для покупки на сумму:({loCost_n:.2f}).',
+          'Пополните счет, пожалуйста.', sep='\n')
+      return False
+    
+    loDesc_s = inp_FltAVali_feif(f' Введите название покупки', laInPTypeFlt_cll=None,
+        laDfV_s="Еда", laAcceptEmptyInPAsDf_b=True)[0]
+    
+    # print(f'DBG: На счету:({loAccSum_n}) и в истории покупок {len(loHstT_l)} зап.')
+    loAccSum_n -= loCost_n
+    loHstT_l.append((loDesc_s, loCost_n)) #DVL: input by inp_FltAVali_feif
+    # print(f'DBG: На счету:({loAccSum_n}) и в истории покупок {len(loHstT_l)} зап.')
+    return True
 
-При выполнении задания можно пользоваться любыми средствами
+  def tVieHst_f():
+    global loAccSum_n, loHstT_l
+    print('', f'История покупок (всего {len(loHstT_l)} зап.):',
+        *enumerate(loHstT_l, 1), '', sep='\n')
 
-Для реализации основного меню можно использовать пример ниже или написать свой
-"""
-glAccSum_n, glHstT_l = 0, []
+  tMenu_d = {'1':('Пополнение счета', tRefillAcc_f),
+      '2':('Покупка', tBuy_f),
+      '3':('История покупок', tVieHst_f),
+      '4':('Выход', None)}
 
-def tRefillAcc_f():
-  global glAccSum_n, glHstT_l
-  loAdd_n = teInPWTypedWVali_fif(f' Ведите сумму на сколько пополнить счет',
-      laInPTypeFlt_cll=float, laDfV_s='100',
-      laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой',
-      laVali_cll=lambda _n: 0 <= _n)[0]
-  glAccSum_n += loAdd_n #DVL: input by teInPWTypedWVali_fif
-  # print(f'DBG: На счету:({glAccSum_n:.2f}) и в истории покупок {len(glHstT_l)} зап.')
-  return True
+  while True:
+      print(*(f'{_k}. {_v[0]}' for _k, _v in tMenu_d.items()), sep='\n')
+      print(f'На счету:({loAccSum_n:.2f}) и в истории покупок {len(loHstT_l)} зап.')
 
-def tBuy_f():
-  global glAccSum_n, glHstT_l
-  loCost_n = teInPWTypedWVali_fif(f' Введите сумму покупки (на Вашем счету:{glAccSum_n:.2f})',
-      laInPTypeFlt_cll=float, laDfV_s=str(min(100, glAccSum_n)),
-      laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой',
-      laVali_cll=lambda _n: 0 <= _n)[0]
+      li_s = input('Выберите пункт меню: ')
+      if li_s in tMenu_d:
+        lo_cll = tMenu_d[li_s][1]
+        if lo_cll is None: break
+        else: loRes_b = lo_cll()
+      else:
+          print(f'Неверный пункт меню:"{li_s}"')
 
-  if glAccSum_n < loCost_n: #DVL: input by teInPWTypedWVali_fif
-    print(f'Денег на Вашем счету:({glAccSum_n:.2f}) не хватает',
-        f'для покупки на сумму:({loCost_n:.2f}).',
-        'Пополните счет, пожалуйста.', sep='\n')
-    return False
-  
-  loDesc_s = teInPWTypedWVali_fif(f' Введите название покупки', laInPTypeFlt_cll=None,
-      laDfV_s="Еда", laAcceptEmptyInPAsDf_b=True)[0]
-  
-  # print(f'DBG: На счету:({glAccSum_n}) и в истории покупок {len(glHstT_l)} зап.')
-  glAccSum_n -= loCost_n
-  glHstT_l.append((loDesc_s, loCost_n)) #DVL: input by teInPWTypedWVali_fif
-  # print(f'DBG: На счету:({glAccSum_n}) и в истории покупок {len(glHstT_l)} зап.')
-  return True
 
-def tVieHst_f():
-  global glAccSum_n, glHstT_l
-  print('', f'История покупок (всего {len(glHstT_l)} зап.):',
-      *enumerate(glHstT_l, 1), '', sep='\n')
 
-tMenu_d = {'1':('Пополнение счета', tRefillAcc_f),
-    '2':('Покупка', tBuy_f),
-    '3':('История покупок', tVieHst_f),
-    '4':('Выход', None)}
-
-while True:
-    print(*(f'{_k}. {_v[0]}' for _k, _v in tMenu_d.items()), sep='\n')
-    print(f'На счету:({glAccSum_n:.2f}) и в истории покупок {len(glHstT_l)} зап.')
-
-    li_s = input('Выберите пункт меню: ')
-    if li_s in tMenu_d:
-      lo_cll = tMenu_d[li_s][1]
-      if lo_cll is None: break
-      else: loRes_b = lo_cll()
-    else:
-        print(f'Неверный пункт меню:"{li_s}"')
-
-print(teSep_s)
+if __name__ == '__main__':
+    import sys
+    # main(sys.argv[1:])
+    main(None)
