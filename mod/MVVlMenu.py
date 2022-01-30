1bf444767ff415666b106a45e33355ab67b0c666
mikev@W10HLEN <dev.mikevvl@outlook.com>
2022-01-30 06:54:16 +0400

    CuW(CleCls(Menu) 2 more Abs & less Imp)

--- mod/MVVlMenu.py
+++ mod/MVVlMenu.py
@@ -13,33 +13,61 @@ from MVVlStd import glSep_s, inp_FltAVali_fefi
 # '''
 # print(glSep_s, tTskMsg_s, f'{sys.version=}', glSep_s, 'Result:', '',  sep='\n')
 
-
+# 2Do:
+# from dataclasses import dataclass, field
+# import copy
+# @dataclass
+# class D:
+#   MenuEls_d: dict = field(default_factory=dict)
+#   InnStt_d: dict = field(default_factory=dict)
+#   ElsFmt_s: str = '{_k!s:>2}. {_v[0]}'
+#   ElFmt_s: str = See(prn_Info_fmp):*(f'{_k}. {self.MenuEls_d[_k][0]}' for _k in self)
+#   InnSttFmt_s: str = None # Df |==''> !OuP |> Sf.format(**tInnStt_d)
+#   # See(prn_Info_fmp)                                                    '({kAccSum_n:{kWid_i}})'.format(**tInnStt_d)
+#   HeaFmt_s: str = None # Df(Rat(glSep_s)) |==''> !OuP |> ??Sf.format(**self.OuPPP_d)
+#   FooFmt_s: str = None # Df(Rat(glSep_s)) |==''> !OuP |> ??Sf.format(**self.OuPPP_d)
+#   # self.OuPPP_d AllEls_co, VieEls_(co|l), MaxWid_i
+#   # x: list = field(default_factory=list)
+#   def __post_init__(self):
+#     # self.MenuEls_d = copy.deepcopy(dict(self.MenuEls_d))
+#     self.MenuEls_d = dict(self.MenuEls_d)
+#     self.InnStt_d = copy.deepcopy(dict(self.InnStt_d))
+#     self.IsRun_b = bool(self.MenuEls_d)
 class MVVlMenu_c():
 
       # laIsKeyExit_cll=lambda _sf, _k: int(_k) == max(iter(_sf))
-  def __init__(self, laMenuEls_d=None,
-      laAccSum_n=0, laHstT_l=None):
+  def __init__(self, MenuEls_d=None, InnStt_d=None, InnSttFmt_s=None,
+      HeaFmt_s=None, FooFmt_s=None, ElsFmt_s='{_k!s:>2}. {_v[0]}'):
     # 2Do: laRet_d{laAccSum_n=0, laHstT_l=None} 4 __call__():return 
-    self.MenuEls_d = dict(laMenuEls_d) if laMenuEls_d is not None else {}
-    self.AccSum_n = int(laAccSum_n)
-    self.HstT_l = laHstT_l if laHstT_l is not None else []
-    # self.IsKeyExit_cll = laIsKeyExit_cll
+    self.MenuEls_d = dict(MenuEls_d) if MenuEls_d is not None else {}
+    self.InnStt_d = dict(InnStt_d) if InnStt_d is not None else {}
+    self.InnSttFmt_s = InnSttFmt_s
+    self.HeaFmt_s = HeaFmt_s
+    self.FooFmt_s = FooFmt_s
+    self.ElsFmt_s = ElsFmt_s
     self.IsRun_b = bool(self.MenuEls_d)
+    # self.kAccSum_n = int(laAccSum_n)
+    # self.kHstT_l = list(laHstT_l) if laHstT_l is not None else []
+    # self.IsKeyExit_cll = laIsKeyExit_cll
 
   def __iter__(self): # 2Do: MaB Onl WhiUse(prn_fmp)
-    return (_el for _el in sorted(self.MenuEls_d.keys(), key=int))
+    return (_k for _k in sorted(self.MenuEls_d.keys(), key=int))
+  def __getitem__(self, _key):
+      return self.MenuEls_d[_key]
 
   # def oup_fmp(self): # 2Do: MaB
   def prn_fmp(self): # 2Do: MaB Onl(9+KeyExit OR Fit2Scr+KeyExit) w/Set(sf.WhiVieElsKey_l)
     if bool(self.MenuEls_d):
       print(glSep_s[:len(glSep_s)//3 *2],
-          *(f'{_k}. {_v[0]}' for _k, _v in self.MenuEls_d.items()),
+          # *(f'{_k}. {self.MenuEls_d[_k][0]}' for _k in self),
+          *(self.ElsFmt_s.format(_k=_k, _v=self[_k]) for _k in self),
+          # *(f'{_k}. {_v[0]}' for _k, _v in self.MenuEls_d.items()),
           glSep_s[:len(glSep_s)//3], sep='\n')
 
   # def oup_Info|Ret_fmp(self):
   # def __str__(self):; __format__; tVieHst_fmp
   def prn_Info_fmp(self):
-    print(f'На счету:({self.AccSum_n:.2f}) и в истории покупок {len(self.HstT_l)} зап.',
+    print(f"На счету:({self.InnStt_d['kAccSum_n']:.2f}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.",
         glSep_s[:len(glSep_s)//3 *2], sep='\n')
 
   # def add_Els?_ffm(self):
@@ -68,26 +96,26 @@ class MVVlMenu_c():
       # self.prn_Info_fmp()
       print('До свидания!', glSep_s[:len(glSep_s)//3 *2], sep='\n')
 
-    return dict(AccSum_n=self.AccSum_n, HstT_l=self.HstT_l)
+    return self.InnStt_d
 
-
+tInnStt_d = dict(kAccSum_n=0, kHstT_l=[])
 def tRefillAcc_fm(self):
   loAdd_n = inp_FltAVali_fefi(f' Ведите сумму на сколько пополнить счет\n',
       laInPTypeFlt_cll=float, laDfV_s='100',
       laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
       laVali_cll=lambda _n: 0 <= _n)[0]
-  self.AccSum_n += loAdd_n #DVL: input by inp_FltAVali_fefi
-  # print(f'DBG: На счету:({self.AccSum_n:.2f}) и в истории покупок {len(self.HstT_l)} зап.')
+  self.InnStt_d['kAccSum_n'] += loAdd_n #DVL: input by inp_FltAVali_fefi
+  # print(f'DBG: На счету:({self.InnStt_d['kAccSum_n']:.2f}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.')
   return True
 
 def tBuy_fmp(self):
-  loCost_n = inp_FltAVali_fefi(f' Введите сумму покупки (на Вашем счету:{self.AccSum_n:.2f})\n',
-      laInPTypeFlt_cll=float, laDfV_s=str(min(100, self.AccSum_n)),
+  loCost_n = inp_FltAVali_fefi(f" Введите сумму покупки (на Вашем счету:{self.InnStt_d['kAccSum_n']:.2f})\n",
+      laInPTypeFlt_cll=float, laDfV_s=str(min(100, self.InnStt_d['kAccSum_n'])),
       laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
       laVali_cll=lambda _n: 0 <= _n)[0]
 
-  if self.AccSum_n < loCost_n: #DVL: input by inp_FltAVali_fefi
-    print(f'Денег на Вашем счету:({self.AccSum_n:.2f}) не хватает',
+  if self.InnStt_d['kAccSum_n'] < loCost_n: #DVL: input by inp_FltAVali_fefi
+    print(f"Денег на Вашем счету:({self.InnStt_d['kAccSum_n']:.2f}) не хватает",
         f' для покупки на сумму:({loCost_n:.2f}).',
         ' Пополните счет, пожалуйста.', sep='\n')
     return False
@@ -95,19 +123,19 @@ def tBuy_fmp(self):
   loDesc_s = inp_FltAVali_fefi(f' Введите название покупки\n', laInPTypeFlt_cll=None,
       laDfV_s="Еда", laAcceptEmptyInPAsDf_b=True)[0]
   
-  # print(f'DBG: На счету:({self.AccSum_n}) и в истории покупок {len(self.HstT_l)} зап.')
-  self.AccSum_n -= loCost_n
-  self.HstT_l.append((loDesc_s, loCost_n)) #DVL: input by inp_FltAVali_fefi
-  # print(f'DBG: На счету:({self.AccSum_n}) и в истории покупок {len(self.HstT_l)} зап.')
+  # print(f'DBG: На счету:({self.InnStt_d['kAccSum_n']}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.')
+  self.InnStt_d['kAccSum_n'] -= loCost_n
+  self.InnStt_d['kHstT_l'].append((loDesc_s, loCost_n)) #DVL: input by inp_FltAVali_fefi
+  # print(f'DBG: На счету:({self.InnStt_d['kAccSum_n']}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.')
   return True
 
 def tVieHst_fmp(self):
-  print(f'История покупок (всего {len(self.HstT_l)} зап.):',
-      *enumerate(self.HstT_l, 1), '', sep='\n')
+  print(f"История покупок (всего {len(self.InnStt_d['kHstT_l'])} зап.):",
+      *enumerate(self.InnStt_d['kHstT_l'], 1), '', sep='\n')
 
 def tExit_fm(self):
   self.IsRun_b = False
-# tMenu_d = {'1':('Пополнение счета', tRefillAcc_fm),
+# tMenu_d = {'1':('Пополнение счета', tRefillAcc_fm, ??Type??(Exit, Back, SbMenu, CtrlVieMenu??)),
 #     '2':('Покупка', tBuy_fm),
 #     '3':('История покупок', tVieHst_fm),
 #     '4':('Выход', None)}
@@ -116,7 +144,7 @@ def main(laArgs: list[str]) -> None:
   tMenu_o = MVVlMenu_c({'1':('Пополнение счета', tRefillAcc_fm),
     '2':('Покупка', tBuy_fmp),
     '3':('История покупок', tVieHst_fmp),
-    '4':('Выход', tExit_fm)})
+    '4':('Выход', tExit_fm)}, tInnStt_d)
   # tMenu_o = MVVlMenu_c()
   # tMenu_o.add_Els?_ffm(...)
   # tRes_d = tMenu_o.run_ffpm()
