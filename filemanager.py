ade22bb8dc08c04a15bc601f32c22f43c1a8ff2b
mikev@W10HLEN <dev.mikevvl@outlook.com>
2022-01-31 21:01:15 +0400

    RFa:(MVVlStd.py(fActHst_l), Prx:f,  MyBankAcc.py)

--- filemanager.py
+++ filemanager.py
@@ -17,12 +17,54 @@
 - выход.
 Так же можно добавить любой дополнительный функционал по желанию.
 """
-import mod.MVVlMenu, mod.MVVlStd, mod.victory, mod.MyBankAcc
+import os, shutil, sys
+import mod.MVVlStd, mod.victory, mod.MyBankAcc
 
+# import os
+# os.makedirs(name, mode=511, exist_ok=False)
+# t_l = os.listdir()
+# os.remove(path, *, dir_fd=None)
+# os.removedirs(name)
+# with os.scandir(path) as it:
+#     for entry in it:
+#         if not entry.name.startswith('.') and entry.is_file():
+#             print(entry.name)
+# os.rename(src, dst, *, src_dir_fd=None, dst_dir_fd=None)
+# os.link(src, dst, *, src_dir_fd=None, dst_dir_fd=None, follow_symlinks=True)
+# os.symlink(src, dst, target_is_directory=False, *, dir_fd=None)
+# os.walk(top, topdown=True, onerror=None, followlinks=False)
+# os.fwalk(top='.', topdown=True, onerror=None, *, follow_symlinks=False, dir_fd=None)
+# os.dup(fd); os.dup2(fd, fd2, inheritable=True)
+
+t_l = ['Создать папку', 'Удалить файл', 'Удалить папку', 'Копировать файл',
+ 'Копировать папку', 'Просмотр содержимого рабочей директории',
+ 'Посмотреть только папки', 'Посмотреть только файлы',
+ 'Просмотр информации об операционной системе', 'Создатель программы',
+ 'Играть в викторину', 'Мой банковский счет', 'Смена рабочей директории',
+ 'Выход']
+tInnStt_d = dict(kRtDir_s=None, kCurDir_s=None, kDirEntry_t=None)
+# InPHstEl:(fCurDirL_ix, fInP_s, fDesc_s, fRes_a)
+
+def tExit_fm(self, file=sys.stdout):
+  self.IsRun_b = False
+
+# tMenu_d = {'1':('Пополнение счета', tRefillAcc_fm, ??Type??(Exit, Back, SbMenu, CtrlVieMenu??)),
+#     '2':('Покупка', tBuy_fm),
+#     '3':('История покупок', tVieHst_fm),
+#     '4':('Выход', None)}
+
+def prn_InnStt_fp(laSf_o, laInnStt_d, file=sys.stdout):
+  # 2Do: CheExs(kAccSum_n, kBuyHstT_l)
+  if 'kAccSum_n' in laInnStt_d and 'kBuyHstT_l' in laInnStt_d:
+    print(f"На счету:({laInnStt_d['kAccSum_n']:.2f})",
+        f"и в истории покупок {len(laInnStt_d['kBuyHstT_l'])} зап.",
+        # glSep_s[:len(glSep_s)//3 *2], sep='\n',
+        file=file)
 
 def main(laArgs: list[str]) -> None:
   # mod.victory.main(None)
-  mod.MyBankAcc.main(None)
+  tRes_d = mod.MyBankAcc.main(None)
+  print(tRes_d)
   # t_l = mod.MVVlStd.glStdMsg4InP_l[:]
   # t_l[0] = 'Выберите'
   # print(t_l[0], mod.MVVlStd.glStdMsg4InP_l[0])
--- mod/MyBankAcc.py
+++ mod/MyBankAcc.py
@@ -7,32 +7,32 @@ from mod.MVVlStd import glSep_s, inp_FltAVali_fefi, Menu_c
 # # inp_FltAVali_fefi('?')
 # from mod.MVVlMenu import Menu_c
 
-tInnStt_d = dict(kAccSum_n=0, kHstT_l=[])
+tInnStt_d = dict(kAccSum_n=0, kBuyHstT_l=[])
 
-def tRefillAcc_fm(self, file=sys.stdout):
+def tRefillAcc_ffmp(self, file=sys.stdout):
   # loAdd_n = inp_FltAVali_fefi(f' Введите сумму на сколько пополнить счет\n',
   loAdd_n = inp_FltAVali_fefi(f' сумму на сколько пополнить счет\n',
       laInPTypeFlt_cll=float, laDfV_s='100.00',
       laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
       laVali_cll=lambda _n: 0 <= _n, file=file)[0]
-  self.InnStt_d['kAccSum_n'] += loAdd_n #DVL: input by inp_FltAVali_fefi
-  # print(f'DBG: На счету:({self.InnStt_d['kAccSum_n']:.2f}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.')
+  self.fInnStt_d['kAccSum_n'] += loAdd_n #DVL: input by inp_FltAVali_fefi
+  # print(f'DBG: На счету:({self.fInnStt_d['kAccSum_n']:.2f}) и в истории покупок {len(self.fInnStt_d['kBuyHstT_l'])} зап.')
   print(f'Пополнение на:({loAdd_n:.2f}).', file=file)
   return loAdd_n
 
-def tBuy_fmp(self, file=sys.stdout):
-  if self.InnStt_d['kAccSum_n'] <= 0:
-    print(f"На Вашем счету:({self.InnStt_d['kAccSum_n']:.2f}) <= 0.",
+def tBuy_ffmp(self, file=sys.stdout):
+  if self.fInnStt_d['kAccSum_n'] <= 0:
+    print(f"На Вашем счету:({self.fInnStt_d['kAccSum_n']:.2f}) <= 0.",
         ' Пополните счет, пожалуйста.', sep='\n', file=file)
     return (None, None)
 
-  loCost_n = inp_FltAVali_fefi(f" сумму покупки (на Вашем счету:{self.InnStt_d['kAccSum_n']:.2f})\n",
-      laInPTypeFlt_cll=float, laDfV_s=f"{min(100.00, self.InnStt_d['kAccSum_n']):.2f}",
+  loCost_n = inp_FltAVali_fefi(f" сумму покупки (на Вашем счету:{self.fInnStt_d['kAccSum_n']:.2f})\n",
+      laInPTypeFlt_cll=float, laDfV_s=f"{min(100.00, self.fInnStt_d['kAccSum_n']):.2f}",
       laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
       laVali_cll=lambda _n: 0 <= _n, file=file)[0]
 
-  if self.InnStt_d['kAccSum_n'] < loCost_n: #DVL: input by inp_FltAVali_fefi
-    print(f"Денег на Вашем счету:({self.InnStt_d['kAccSum_n']:.2f}) не хватает",
+  if self.fInnStt_d['kAccSum_n'] < loCost_n: #DVL: input by inp_FltAVali_fefi
+    print(f"Денег на Вашем счету:({self.fInnStt_d['kAccSum_n']:.2f}) не хватает",
         f' для покупки на сумму:({loCost_n:.2f}).',
         ' Пополните счет, пожалуйста.', sep='\n', file=file)
     return (None, loCost_n)
@@ -40,40 +40,44 @@ def tBuy_fmp(self, file=sys.stdout):
   loDesc_s = inp_FltAVali_fefi(f' название покупки\n', laInPTypeFlt_cll=None,
       laDfV_s="Еда", laAcceptEmptyInPAsDf_b=True, file=file)[0]
   
-  # print(f'DBG: На счету:({self.InnStt_d['kAccSum_n']}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.')
-  self.InnStt_d['kAccSum_n'] -= loCost_n
-  self.InnStt_d['kHstT_l'].append((loDesc_s, loCost_n)) #DVL: input by inp_FltAVali_fefi
-  # print(f'DBG: На счету:({self.InnStt_d['kAccSum_n']}) и в истории покупок {len(self.InnStt_d['kHstT_l'])} зап.')
+  # print(f'DBG: На счету:({self.fInnStt_d['kAccSum_n']}) и в истории покупок {len(self.fInnStt_d['kBuyHstT_l'])} зап.')
+  self.fInnStt_d['kAccSum_n'] -= loCost_n
+  self.fInnStt_d['kBuyHstT_l'].append((loDesc_s, loCost_n)) #DVL: input by inp_FltAVali_fefi
+  # print(f'DBG: На счету:({self.fInnStt_d['kAccSum_n']}) и в истории покупок {len(self.fInnStt_d['kBuyHstT_l'])} зап.')
   print(f'Покупка: "{loDesc_s}", на сумму:({loCost_n:.2f}).', file=file)
   return (loDesc_s, loCost_n)
 
-def tVieHst_fmp(self, file=sys.stdout):
-  print(f"История покупок (всего {len(self.InnStt_d['kHstT_l'])} зап.):",
-      *enumerate(self.InnStt_d['kHstT_l'], 1), '', sep='\n', file=file)
-  return (self.InnStt_d['kAccSum_n'], len(self.InnStt_d['kHstT_l']))
+def tVieHst_ffmp(self, file=sys.stdout):
+  print(f"История покупок (всего {len(self.fInnStt_d['kBuyHstT_l'])} зап.):",
+      *enumerate(self.fInnStt_d['kBuyHstT_l'], 1), '', sep='\n', file=file)
+  return (self.fInnStt_d['kAccSum_n'], len(self.fInnStt_d['kBuyHstT_l']))
 
 def tExit_fm(self, file=sys.stdout):
-  self.IsRun_b = False
+  self.fIsRun_b = False
 
-# tMenu_d = {'1':('Пополнение счета', tRefillAcc_fm, ??Type??(Exit, Back, SbMenu, CtrlVieMenu??)),
+# tMenu_d = {'1':('Пополнение счета', tRefillAcc_ffmp, ??Type??(Exit, Back, SbMenu, CtrlVieMenu??)),
 #     '2':('Покупка', tBuy_fm),
 #     '3':('История покупок', tVieHst_fm),
 #     '4':('Выход', None)}
 
-def prn_InnStt_fmp(self, laInnStt_d, file=sys.stdout):
-  print(f"На счету:({laInnStt_d['kAccSum_n']:.2f}) и в истории покупок {len(laInnStt_d['kHstT_l'])} зап.",
-      # glSep_s[:len(glSep_s)//3 *2], sep='\n',
-      file=file)
+def prn_InnStt_fp(laSf_o, laInnStt_d, file=sys.stdout):
+  # 2Do: CheExs(kAccSum_n, kBuyHstT_l)
+  if 'kAccSum_n' in laInnStt_d and 'kBuyHstT_l' in laInnStt_d:
+    print(f"На счету:({laInnStt_d['kAccSum_n']:.2f})",
+        f"и в истории покупок {len(laInnStt_d['kBuyHstT_l'])} зап.",
+        # glSep_s[:len(glSep_s)//3 *2], sep='\n',
+        file=file)
 
 def main(laArgs: list[str]) -> None:
   # Ww(sys.argv[1:])
-  tMenu_o = Menu_c({1:('Пополнение счета', tRefillAcc_fm),
-    '2':('Покупка', tBuy_fmp),
-    '3':('История покупок', tVieHst_fmp),
+  tMenu_o = Menu_c({1:('Пополнение счета', tRefillAcc_ffmp),
+    '2':('Покупка', tBuy_ffmp),
+    '3':('История покупок', tVieHst_ffmp),
     # 'E':('Выход', tExit_fm),
     '4':('Выход', tExit_fm)
-    }, InnStt_d=tInnStt_d, PrnInnStt_fmp=prn_InnStt_fmp,
-    HeaFmt_s= glSep_s[:len(glSep_s)//3 *2] + '\nМой банковский счет:')
+    }, fInnStt_d=tInnStt_d, fPrnInnStt_cll=prn_InnStt_fp,
+    fHeaFmt_s= glSep_s + '\nМой банковский счет:')
+    # HeaFmt_s= glSep_s[:len(glSep_s)//3 *2] + '\nМой банковский счет:')
   # tMenu_o = Menu_c()
   # tMenu_o.add_Itm?_ffm(...)
   # tRes_d = tMenu_o.run_ffpm()
