709ed4386c141bb2686ac8a0e9a9d65474a203cb
mikev@W10HLEN <dev.mikevvl@outlook.com>
2022-06-25 20:14:03 +0400

    Add&Use:mCre_SFrFloat_ff; RFa

--- mod/MyBankAcc.py
+++ mod/MyBankAcc.py
@@ -1,34 +1,45 @@
 import copy, sys
-from mod.MVVlStd import glSep_s, mInP_FltAVali_fefi, mMenu_c, mSupportsWrite_ca
+from mod.MVVlStd import (glSep_s, mInP_FltAVali_fefi, mMenu_c, mSupportsWrite_ca,
+    mCre_SFrFloat_ff)
 # from mod.MVVlStd import glSep_s, mInP_FltAVali_fefi, mMenu_c
 
 mOutStt_d = dict(kAccSum_n=0, kBuyHstT_l=[])
 
 def mA_RefillAcc_ffmp(laSf_o, file=sys.stdout):
   # loAdd_n = mInP_FltAVali_fefi(f' Введите сумму на сколько пополнить счет\n',
+  lo_s = 'положительное число,\n например: (10), (1_000,33), (100.15) или (1000,55)\n'
   loAdd_n = mInP_FltAVali_fefi(f' сумму на сколько пополнить счет\n',
-      laInPTypeFlt_cll=float, laDfV_s='100.00',
-      laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
+      laInPTypeFlt_cll=lambda _s: float(_s.replace(',', '.')),
+      laDfV_s=mCre_SFrFloat_ff(100),
+      # laInPTypeFlt_cll=float, laDfV_s='100.00',
+      laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=lo_s,
+      # laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
       laVali_cll=lambda _n: 0 <= _n, file=file)[0]
   mOutStt_d['kAccSum_n'] += loAdd_n #DVL: input by mInP_FltAVali_fefi
   # print(f'DBG: На счету:({mOutStt_d['kAccSum_n']:.2f}) и в истории покупок {len(mOutStt_d['kBuyHstT_l'])} зап.')
-  print(f'Пополнение на:({loAdd_n:.2f}).', file=file)
+  # lo_s = (f"{loAdd_n:_f}").replace('.', ',', 1).rstrip('0')
+  print(f'Пополнение на:({mCre_SFrFloat_ff(loAdd_n)}).', file=file)
   return loAdd_n
 
 def mA_Buy_ffmp(laSf_o, file=sys.stdout):
   if mOutStt_d['kAccSum_n'] <= 0:
-    print(f"На Вашем счету:({mOutStt_d['kAccSum_n']:.2f}) <= 0.",
+    print(f"На Вашем счету:({mCre_SFrFloat_ff(mOutStt_d['kAccSum_n'])}) <= 0.",
         ' Пополните счет, пожалуйста.', sep='\n', file=file)
     return (None, None)
 
-  loCost_n = mInP_FltAVali_fefi(f" сумму покупки (на Вашем счету:{mOutStt_d['kAccSum_n']:.2f})\n",
-      laInPTypeFlt_cll=float, laDfV_s=f"{min(100.00, mOutStt_d['kAccSum_n']):.2f}",
-      laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=f'положительное число с возм.десят.точкой\n',
+  lo_s = 'положительное число,\n например: (10), (1_000,33), (100.15) или (1000,55)\n'
+  loCost_n = mInP_FltAVali_fefi((" сумму покупки (на Вашем счету:" +
+      f"{mCre_SFrFloat_ff(mOutStt_d['kAccSum_n'])})\n"),
+      laInPTypeFlt_cll=lambda _s: float(_s.replace(',', '.')),
+      laDfV_s=mCre_SFrFloat_ff(min(100.00, mOutStt_d['kAccSum_n'])),
+      # laDfV_s=(f"{min(100.00, mOutStt_d['kAccSum_n']):_f}").replace('.', ',', 1).rstrip('0'),
+      # laDfV_s=f"{min(100.00, mOutStt_d['kAccSum_n']):.2f}",
+      laAcceptEmptyInPAsDf_b=True, laValiInPMsg_s=lo_s,
       laVali_cll=lambda _n: 0 <= _n, file=file)[0]
 
   if mOutStt_d['kAccSum_n'] < loCost_n: #DVL: input by mInP_FltAVali_fefi
-    print(f"Денег на Вашем счету:({mOutStt_d['kAccSum_n']:.2f}) не хватает",
-        f' для покупки на сумму:({loCost_n:.2f}).',
+    print(f"Денег на Вашем счету:({mCre_SFrFloat_ff(mOutStt_d['kAccSum_n'])})",
+        f' не хватает для покупки на сумму:({mCre_SFrFloat_ff(loCost_n)}).',
         ' Пополните счет, пожалуйста.', sep='\n', file=file)
     return (None, loCost_n)
   
@@ -39,7 +50,7 @@ def mA_Buy_ffmp(laSf_o, file=sys.stdout):
   mOutStt_d['kAccSum_n'] -= loCost_n
   mOutStt_d['kBuyHstT_l'].append((loDesc_s, loCost_n)) #DVL: input by mInP_FltAVali_fefi
   # print(f'DBG: На счету:({mOutStt_d['kAccSum_n']}) и в истории покупок {len(mOutStt_d['kBuyHstT_l'])} зап.')
-  print(f'Покупка: "{loDesc_s}", на сумму:({loCost_n:.2f}).', file=file)
+  print(f'Покупка: "{loDesc_s}", на сумму:({mCre_SFrFloat_ff(loCost_n)}).', file=file)
   return (loDesc_s, loCost_n)
 
 def mA_VieHst_ffmp(laSf_o, file=sys.stdout):
@@ -60,7 +71,7 @@ def mOuP_Stt_fmp(laSf_o:mMenu_c, file:mSupportsWrite_ca=sys.stdout):
 # def mOuP_Stt_fmp(laSf_o, file=sys.stdout):
   # 2Do: CheExs(kAccSum_n, kBuyHstT_l)
   if 'kAccSum_n' in mOutStt_d and 'kBuyHstT_l' in mOutStt_d:
-    print(f"На счету:({mOutStt_d['kAccSum_n']:.2f})",
+    print(f"На счету:({mCre_SFrFloat_ff(mOutStt_d['kAccSum_n'])})",
         f"и в истории покупок {len(mOutStt_d['kBuyHstT_l'])} зап.",
         # glSep_s[:len(glSep_s)//3 *2], sep='\n',
         file=file)
